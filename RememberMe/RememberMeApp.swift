f6e51712df86cb18ab8bbf29256fc59628d4188b
Chris Fouts <cbfouts@micromat.com>
2021-11-18 11:18:16 -0800

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Christopher Fouts on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
